005ac67482e6dc3b2d6f5e26961130906a84c434
+ <+>
2024-04-06 10:37:52 +0300

    Program that generates arrays in between

--- arrayNumbers.js
+++ arrayNumbers.js
@@ -1,4 +1,4 @@
-const prompt = require('prompt-sync')({ sigint: true }); // Import prompt-sync for user input
+const prompt = require('prompt-sync')({ sigint: true }); 
 
 function generateArrayBetween(start, end) {
   // Validate input (start should be less than or equal to end)
@@ -7,7 +7,7 @@ function generateArrayBetween(start, end) {
     return 
   }
 
-  const result = [];
+  const result = []; //Create a blank array where values in between orignal values will enter 
   for (let i = start; i <= end; i++) {
     result.push(i);
   }
@@ -20,6 +20,7 @@ const numbers = [];
 numbers[0] = (prompt("Enter the first number: "));
 numbers[1] = (prompt("Enter the second number: "));
 
+//Call our function and pass the index of the user input as arguments (the first for start and the second for stop ) to initialize the incrimentation in the function
 const generatedArray = generateArrayBetween(numbers[0], numbers[1]);
 
 console.log("The generated array is:", generatedArray);
